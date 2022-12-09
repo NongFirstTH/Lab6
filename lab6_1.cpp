6ffc893351fef71c5fa6b22753d5cd1c043c6a5b
NongFirstTH <TanabodeeChannel@gmail.com>
2022-12-09 18:25:39 +0700

    lab6_1.cpp completed

---
+++ lab6_1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+    int even=0,odd=0;
+    while(num!=0){
+        cout << "Enter an integer: ";
+        cin >> num;
+            if(num%2==0){
+                even++;
+                }
+            else{
+                odd++;
+                }
+        }
+        cout << "#Even numbers = " << even -1 <<endl;
+        cout << "#Odd numbers = " << odd;
+    return 0;
+}
